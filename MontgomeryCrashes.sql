d23f5efe1abd84aa7a00724b0438911423662c52
LeasingFall <160304449+LeasingFall@users.noreply.github.com>
2024-12-06 11:02:10 -0600

    Updated MontgomeryCrashes.sql

--- MontgomeryCrashes.sql
+++ MontgomeryCrashes.sql
@@ -1,42 +1,22 @@
-create table trafficViolations (
-    "seqid" INTEGER,
-    "date" INTEGER, -- Check DATE type option format: 'YYYY-MM-DD' and > < = works on this 
-    "time" INTEGER,
-    "description" TEXT,
-    "vid" INTEGER,
-    "pid" INTEGER,
-    PRIMARY KEY ("seqid"),
-    FOREIGN KEY ("pid") REFERENCES "driver"("pid"),
-    FOREIGN KEY ("vid") REFERENCES "vehicle"("vid")
-);
-
-create table person (
-    "pid" INTEGER,
-    "injurySeverity" TEXT,
-    "atFault" BOOLEAN;
-    "substanceAbuse" TEXT,
-    PRIMARY KEY ("pid"),
-);
+drop table if exists relatedWith;
+drop table if exists Drives;
+drop table if exists has;
+drop table if exists collision;
+drop table if exists report;
+drop table if exists environmentalConditions;
+drop table if exists nonMotorists;
+drop table if exists trafficViolations;
+drop table if exists location;
+drop table if exists vehicle;
+drop table if exists driver;
 
 create table driver (
-    "pid" INTEGER PRIMARY KEY,
-    "distractedBy" TEXT,
-    FOREIGN KEY ("pid") REFERENCES "person"("pid")
-);
-
-create table drives (
     "pid" INTEGER,
-    "vid" INTEGER,
-    FOREIGN KEY ("pid") REFERENCES "driver"("pid"),
-    FOREIGN KEY ("vid") REFERENCES "vehicle"("vid")
-);
-
-create table nonMotorists (
-    "pid" INTEGER PRIMARY KEY,
-    "type" TEXT,
-    "actions" TEXT,
-    "movement" TEXT,
-    FOREIGN KEY ("pid") REFERENCES "person"("pid")
+    "injurySeverity" TEXT,
+    "atFault" TEXT,
+    "substanceAbuse" TEXT,
+    "distractedBy" TEXT,
+     PRIMARY KEY ("pid")
 );
 
 create table vehicle (
@@ -58,29 +38,50 @@ create table location (
     "routeType" TEXT,
     "vehicleGoingDir" TEXT,
     "crossStreet" TEXT,
-    PRIMARY KEY ("longitude", "latitude")
+    PRIMARY KEY ("latitude", "longitude")
 );
 
-create table has ( -- between environmental conditions and location
+create table trafficViolations (
+    "seqid" INTEGER,
+    "date" INTEGER, -- Check DATE type option format: 'YYYY-MM-DD' and > < = works on this 
+    "time" INTEGER,
+    "description" TEXT,
+    "outcome" TEXT,
+    "vid" INTEGER,
+    "pid" INTEGER,
     "longitude" DECIMAL(10,8),
     "latitude" DECIMAL(10,8),
-	"ecid" INTEGER,
-	PRIMARY KEY("longitude","latitude", "ecid"),
-	FOREIGN KEY("ecid") REFERENCES "environmentalConditions"("ecid"),
-	FOREIGN KEY("longitude") REFERENCES "location"("longitude"),
-	FOREIGN KEY("latitude") REFERENCES "location"("latitude")
+    PRIMARY KEY ("seqid"),
+    FOREIGN KEY ("pid") REFERENCES "driver"("pid"),
+    FOREIGN KEY ("vid") REFERENCES "vehicle"("vid"),
+    FOREIGN KEY ("latitude", "longitude") REFERENCES "location"("latitude", "longitude")
+);
+
+create table nonMotorists (
+    "pid" INTEGER PRIMARY KEY,
+    "type" TEXT,
+    "actions" TEXT,
+    "movement" TEXT,
+    "injurySeverity" TEXT,
+    "atFault" TEXT,
+    "substanceAbuse" TEXT
 );
 
 create table environmentalConditions (
-    "ecid" INTEGER AUTOINCREMENT,
+    "ecid" INTEGER PRIMARY KEY IDENTITY(1,1),
     "weather" TEXT,
     "surfaceCondition" TEXT,
-    "lightCondition" TEXT,
-    PRIMARY KEY ("ecid")
+    "lightCondition" TEXT
+);
+
+create table report (
+    "reportNumber" INTEGER,
+    "reportType" TEXT,
+    PRIMARY KEY ("reportNumber") 
 );
 
 create table collision (
-    "cid" INTEGER AUTOINCREMENT,
+    "cid" INTEGER PRIMARY KEY IDENTITY(1,1),
     "crashDate" INTEGER,
     "crashTime" INTEGER,
     "longitude" DECIMAL(10,8),
@@ -90,12 +91,26 @@ create table collision (
     "reportNumber" INTEGER,
     "firstImpactLocation" TEXT,
     "damageExtent" TEXT,
-    "collisionExtent" TEXT,
-    PRIMARY KEY ("cid"),
-    FOREIGN KEY("longitude") REFERENCES "location"("longitude"),
-	FOREIGN KEY("latitude") REFERENCES "location"("latitude"),
+    "collisionType" TEXT,
+    FOREIGN KEY ("latitude", "longitude") REFERENCES "location"("latitude", "longitude"),
 	FOREIGN KEY("reportNumber") REFERENCES "report"("reportNumber"),
 	FOREIGN KEY("pid") REFERENCES "driver"("pid")
+);
+
+create table has ( -- between environmental conditions and location
+    "longitude" DECIMAL(10,8),
+    "latitude" DECIMAL(10,8),
+	"ecid" INTEGER,
+	PRIMARY KEY("longitude","latitude", "ecid"),
+	FOREIGN KEY("ecid") REFERENCES "environmentalConditions"("ecid"),
+	FOREIGN KEY ("latitude", "longitude") REFERENCES "location"("latitude", "longitude")
+);
+
+create table drives (
+    "pid" INTEGER,
+    "vid" INTEGER,
+    FOREIGN KEY ("pid") REFERENCES "driver"("pid"),
+    FOREIGN KEY ("vid") REFERENCES "vehicle"("vid")
 );
 
 create table relatedWith ( -- Between nonMotorist and collisions
@@ -103,11 +118,5 @@ create table relatedWith ( -- Between nonMotorist and collisions
     "cid" INTEGER,
     PRIMARY KEY ("cid", "pid"),
     FOREIGN KEY("cid") REFERENCES "collision"("cid"),
-    FOREIGN KEY("pid") REFERENCES "nonMotorist"("pid")
-);
-
-create table report (
-    "reportNumber" INTEGER,
-    "reportType" TEXT,
-    PRIMARY KEY ("reportNumber") 
+    FOREIGN KEY("pid") REFERENCES "nonMotorists"("pid")
 );
